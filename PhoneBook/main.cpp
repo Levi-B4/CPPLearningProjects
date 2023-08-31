23b8fa2ea78c01193635e597e2fbf19c85b11caa
Levi-B4 <levi.butler4@gmail.com>
2023-08-31 13:08:08 -0400

    created outline of phonebook

---
+++ PhoneBook/main.cpp
@@ -0,0 +1,13 @@
+#include “search_functions.h”
+
+using namespace std;
+
+int main() {
+    // Read in File
+     do {
+        // prompt user for options
+        // prompt user for query
+        // search
+        // display results
+    } while (option != 5)
+}
---
+++ PhoneBook/search_functions.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_FUNCTIONS
+#define SEARCH_FUNCTIONS
+
+using std::vector;
+using std::string;
+
+int searchRecursive(vector<string> &data, string query);
+
+int searchIterative(vector<string> &data, string query);
+
+#endif
