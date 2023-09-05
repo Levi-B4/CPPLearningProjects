ce097e3706502e6d6dda29cec47c4657aae1b1d8
Levi-B4 <levi.butler4@gmail.com>
2023-09-05 12:03:50 -0400

    Added menu loop and a parser for the file input

--- PhoneBook/search_functions.cpp
+++ PhoneBook/search_functions.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
-
+//searches for query inside of a vector using a recursive function
 int searchRecursive(std::vector<std::string> &data, std::string query)
 {
-// implementation
+    cout << "Search Recursive" << endl;
 }
+//searches for query inside of a vector using an iterative function
 int searchIterative(std::vector<std::string> &data, std::string query)
 {
-// implementation
+    cout << "Search Iterative" << endl;
 }
